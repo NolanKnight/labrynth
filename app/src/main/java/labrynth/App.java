8a638dd89acc3c9e7115336c2b4769087edf211c
NolanKnight <nolan.r.knight@gmail.com>
2024-07-05 16:51:42 -0700

    removed unnecessary code

--- app/src/main/java/labrynth/App.java
+++ app/src/main/java/labrynth/App.java
@@ -3,13 +3,7 @@
  */
 package labrynth;
 
-import javax.swing.JFrame;
-
 public class App {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
