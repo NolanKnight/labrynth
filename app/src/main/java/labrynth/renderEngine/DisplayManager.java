99854841e754b9dda09f45745ba636a50f712566
NolanKnight <nolan.r.knight@gmail.com>
2024-07-05 17:18:28 -0700

    added title

--- app/src/main/java/labrynth/renderEngine/DisplayManager.java
+++ app/src/main/java/labrynth/renderEngine/DisplayManager.java
@@ -21,6 +21,7 @@ public class DisplayManager {
         try {
             Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
             Display.create(new PixelFormat(), attribs);
+            Display.setTitle("Labrynth");
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
